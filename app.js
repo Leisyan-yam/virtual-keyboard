6ce52a4e596a9e71c9daaa167559e51cfc6cd7e4
Leisyan Yamaltdinova <n.leisyan@yandex.ru>
2023-05-01 00:04:59 +0300

    feat: add backspace option

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 let arr2 = ['Backspace', 'Tab', 'CapsLock', ' ', 'Meta', 'Shift', 'Enter', '\\']
-
+const specials = [ "Tab", "CapsLock", "ShiftLeft", "ControlLeft", "MetaLeft", "AltLeft", "AltRight", "ControlRight", "ArrowLeft", "ArrowUp", "ArrowDown", "ArrowRight", "ShiftRight", "Enter", "Backspace" ]
 
 let arr =[];
+
+
+keyArr = [];
+
+document.onkeydown = (event) => {
+  console.log(event.code);
+  keyArr.push(event.code);
+  console.log(keyArr)
+}
 
 const keyboardContainer = document.createElement("div");
 keyboardContainer.classList.add("main-container");
@@ -9,11 +18,7 @@ document.body.appendChild(keyboardContainer);
 
 const inputField = document.createElement("textarea");
 inputField.classList.add("input");
-document.body.insertBefore(inputField, keyboardContainer)
-
-let text =[]
-
-
+document.body.insertBefore(inputField, keyboardContainer);
 
 
 
@@ -30,7 +35,7 @@ data.forEach(el => {
   const char = document.createElement("div");
   char.classList.add("char");
   char.textContent = `${(item.key)}`;
-  char.setAttribute('data', `${item.code}`)
+  char.setAttribute("code", `${item.code}`)
       if (item.code === "MetaLeft") {
         char.textContent = "Win";
       }
@@ -70,28 +75,44 @@ data.forEach(el => {
       if(item.key === ' ') {
         char.classList.add("space")
       }
-
-      arr.push(char)
-
       keyboardContainer.appendChild(row);
       row.appendChild(char);
-
+})})
 
 document.onkeydown = (event) => {
   document.querySelectorAll(".char").forEach((el) => {
     el.classList.remove("active")
   })
-  document.querySelector(`.char[data = ${event.code}]`).classList.add("active")
+  document.querySelector(`.char[code = ${event.code}]`).classList.add("active");
 }
+
 
 document.querySelectorAll(".char").forEach((el) => {
   el.addEventListener("click", () =>  {
     document.querySelectorAll(".char").forEach((item) => {
-    item.classList.remove("active")
-    el.classList.add("active")})
+    item.classList.remove("active");
+    el.classList.add("active")});
+    if (!(specials.includes(el.getAttribute("code")))){
+    arr.push(el.textContent);
+    inputField.innerText = arr.join("")
+  }
+  if(el.getAttribute("code") === "Backspace") {
+    arr.pop();
+    inputField.innerText = arr.join("")
+  }
   })
 })
 
-})})
+
+document.querySelector(".char[code = 'Backspace']").addEventListener("click", () => {
+arr = inputField.value.split('');
+console.log(arr)
+arr.pop();
+console.log(arr)
+inputField.value= arr.join("")
+})
+
+
+
 }
 getData();
